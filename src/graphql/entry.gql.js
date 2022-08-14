401346c69a014e0a700e368a6e73b750ce1cfc86
Angelo Keirsebilck <angelokeirsebilck@Mac-Pro-van-Angelo.local>
2022-08-14 15:20:46 +0200

    SSR v1

---
+++ src/graphql/entry.gql.js
@@ -0,0 +1,15 @@
+import { gql } from "graphql-request";
+
+export const entryQuery = gql`
+  query entryQuery($uri: [String]!, $siteId: [QueryArgument]!) {
+    entry(uri: $uri, siteId: $siteId) {
+      id
+      title
+      uri
+      slug
+      typeHandle
+      sectionHandle
+      siteId
+    }
+  }
+`;
