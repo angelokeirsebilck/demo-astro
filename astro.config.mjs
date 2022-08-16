d7b3bd478d7dca8561d1efaf182b008e40db0ee9
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-08-16 12:55:51 +0200

    Add netloify server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,27 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  integrations: [tailwind()],
+export default defineConfig((mode) => {
+  const { PUBLIC_SITE, PUBLIC_LIVE_PREVIEW } = loadEnv(mode, process.cwd(), "");
+  const settings = {
+    integrations: [tailwind()],
+    site: PUBLIC_SITE,
+    build: {
+      format: "file",
+    },
+    server: { port: 3002, host: true },
+  };
+
+  // If Live Preview true do SSR
+  if (PUBLIC_LIVE_PREVIEW === "true") {
+    settings["output"] = "server";
+    settings["adapter"] = netlify();
+  } else {
+    settings["output"] = "static";
+  }
+
+  return settings;
 });
